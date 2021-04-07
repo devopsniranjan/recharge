6088beb9b4677bb136bdafa44c6e5a1b7e28be57
niranjan <niranjan@devops.com>
2021-04-07 17:15:16 +0530

    recharge

---
+++ core/src/main/kotlin/hdfc/core/generated/Generated.kt
@@ -0,0 +1,29 @@
+package hdfc.core.generated
+
+import ws.osiris.aws.ApiFactory
+import ws.osiris.aws.ApplicationConfig
+import ws.osiris.aws.ProxyLambda
+import ws.osiris.core.Api
+import ws.osiris.core.ComponentsProvider
+
+import hdfc.core.api
+import hdfc.core.createComponents
+
+/**
+ * The lambda function that is deployed to AWS and provides the entry point to the application.
+ *
+ * This is generated code and should not be modified or deleted.
+ */
+@Suppress("UNCHECKED_CAST", "unused")
+class GeneratedLambda : ProxyLambda<ComponentsProvider>(api as Api<ComponentsProvider>, createComponents())
+
+/**
+ * Creates the API and application configuration; used by the build plugins during deployment.
+ *
+ * This is generated code and should not be modified or deleted.
+ */
+@Suppress("UNCHECKED_CAST", "unused")
+class GeneratedApiFactory<T : ComponentsProvider> : ApiFactory<T> {
+    override val api: Api<T> = hdfc.core.api as Api<T>
+    override val config: ApplicationConfig = hdfc.core.config
+}
